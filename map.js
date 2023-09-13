eac5c0b99ebe1c2557ddfba4554b3089e4bdc75b
SyedYaseenUddin <57957691+SyedYaseenUddin@users.noreply.github.com>
2023-09-13 13:16:00 +0530

    3 files modified

--- map.js
+++ map.js
@@ -1,23 +1,93 @@
 class LeafletMap {
-  
   constructor(elementId) {
     this.map = null;
+    this.heatLayer = null;
+    this.markers = null;
+    this.markerCluster = null;
     this.elementId = elementId;
     const element = document.querySelector(`#${this.elementId}`);
     if (!element) {
-      throw 'Element does not exist.'
+      throw 'Element does not exist.';
     }
 
+    this.latlanData = [
+      {
+        lat: 0.5,
+        lon: 37.5,
+        intensity: 0.5,
+        popup: 'Point 1 - Some information here',
+      },
+      { lat: 0.6, lon: 37.6, intensity: 0.8, popup: 'Point 2 - More details' },
+      {
+        lat: 0.7,
+        lon: 37.7,
+        intensity: 1.0,
+        popup: 'Point 3 - Additional info',
+      },
+    ];
     this.drawMap();
-
+    this.updateMapLayers();
   }
 
   drawMap() {
     this.map = L.map(this.elementId).setView([0, 37.9], 6);
     L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-            attribution: '&copy; <a href="https://www.google.com/maps">Google Maps</a>'
-        }).addTo(this.map);
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      attribution:
+        '&copy; <a href="https://www.google.com/maps">Google Maps</a>',
+    }).addTo(this.map);
+  }
+
+  drawHeatLayer() {
+    this.heatLayer = L.heatLayer(
+      this.latlanData.map(function (point) {
+        return [point.lat, point.lon, point.intensity];
+      }),
+      {
+        radius: 25,
+        blur: 15,
+        maxZoom: 10,
+      }
+    ).addTo(this.map);
+  }
+
+  removeHeatLayer() {
+    if (this.heatLayer) {
+      this.map.removeLayer(this.heatLayer);
+      this.heatLayer = null;
+    }
+  }
+
+  drawMarker() {
+    this.markers = this.latlanData.map((point) => {
+      const marker = L.marker([point.lat, point.lon]);
+      marker.bindPopup(point.popup);
+      return marker;
+    });
+
+    this.markerCluster = L.markerClusterGroup();
+    this.markerCluster.addLayers(this.markers);
+    this.map.addLayer(this.markerCluster);
+  }
+
+  removeMarkers() {
+    if (this.markers && this.markerCluster) {
+      this.markerCluster.removeLayer(this.markers);
+      this.map.removeLayer(this.markerCluster);
+      this.markers = null;
+      this.markerCluster = null;
+    }
+  }
+
+  updateMapLayers() {
+    const zoomLevel = this.map.getZoom();
+    if (zoomLevel < 3) {
+      this.removeHeatLayer();
+      this.drawMarker();
+    } else {
+      this.removeMarkers();
+      this.drawHeatLayer();
+    }
   }
 }
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 $(document).ready(function(){
+  const leaflet = new LeafletMap('map');
 });
